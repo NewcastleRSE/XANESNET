e0b9598d35c7dcc9c041e387f1d8bc8a115b29f6
Junkiii <hendrik@junkawitsch.de>
2026-01-06 13:07:15 +0000

    More registry

---
+++ xanesnet/registry/model.py
@@ -0,0 +1,39 @@
+"""
+XANESNET
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either Version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+class ModelRegistry:
+    _registry = {}
+
+    @classmethod
+    def register(cls, name: str):
+        def decorator(ds_cls):
+            if name in cls._registry:
+                raise KeyError(f"Model '{name}' already registered")
+            cls._registry[name] = ds_cls
+            return ds_cls
+
+        return decorator
+
+    @classmethod
+    def get(cls, name: str):
+        if name not in cls._registry:
+            raise KeyError(f"Model '{name}' not found in registry")
+        return cls._registry[name]
+
+    @classmethod
+    def list(cls):
+        return list(cls._registry.keys())
