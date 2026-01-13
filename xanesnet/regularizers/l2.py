2c7acebda24bea52aaeefa6d5612840c9e4b2d22
Junkiii <hendrik@junkawitsch.de>
2026-01-13 08:40:37 +0000

    cleanly added regularizers

---
+++ xanesnet/regularizers/l2.py
@@ -0,0 +1,34 @@
+"""
+XANESNET
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either Version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import torch
+
+from .base import Regularizer
+from .registry import RegularizerRegistry
+
+
+@RegularizerRegistry.register("l2")
+class L2Reg(Regularizer):
+    """
+    L2 regularization (sum of squared parameter values)
+    """
+
+    def __init__(self, type: str):
+        super().__init__(type)
+
+    def forward(self, model):
+        params = torch.cat([p.view(-1) for p in model.parameters()])
+        return torch.norm(params, p=2)
---
+++ xanesnet/regularizers/no.py
@@ -0,0 +1,33 @@
+"""
+XANESNET
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either Version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import torch
+
+from .base import Regularizer
+from .registry import RegularizerRegistry
+
+
+@RegularizerRegistry.register("no")
+class NoReg(Regularizer):
+    """
+    No regularization
+    """
+
+    def __init__(self, type: str):
+        super().__init__(type)
+
+    def forward(self, model):
+        return torch.tensor(0.0, device=next(model.parameters()).device)
