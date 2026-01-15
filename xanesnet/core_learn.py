38077cab74ebaf5cfcc472da592251a5e24ac5ad
Junkiii <hendrik@junkawitsch.de>
2026-01-15 10:41:02 +0000

    adapted core learning to use strategies and learners

--- xanesnet/core_learn.py
+++ xanesnet/core_learn.py
@@ -25,9 +25,10 @@ from torchinfo import summary
 
 from xanesnet.datasets import Dataset, DatasetRegistry
 from xanesnet.datasources import DataSource, DataSourceRegistry
-from xanesnet.models import Model, ModelRegistry, PretrainedModels
-from xanesnet.scheme import Learn, SchemeRegistry
-from xanesnet.utils.io import load_pretrained_model, save_models
+from xanesnet.learners import Learner, LearnerRegistry
+from xanesnet.models import Model
+from xanesnet.strategies import Strategy, StrategyRegistry
+from xanesnet.utils.io import save_models
 from xanesnet.utils.mode import Mode, get_mode
 
 ###############################################################################
@@ -44,25 +45,27 @@ def train(config, args):
 
     datasource = _setup_datasource(config)
     dataset = _setup_dataset(config, mode, datasource)
-    model = _setup_model(config, dataset)
-    scheme = _setup_scheme(config, args, model, dataset)
+    strategy = _setup_strategy(config, dataset)
+    strategy.setup_models()
+    strategy.setup_learners()
 
     # Main training
-    model_list, scheme_type, train_time = _run_training(config, scheme)
+    model_list, train_time = _run_training(strategy)
 
     # Display model summary and training duration
     _summary_model(model_list[0], dataset)
     logging.info(f"Training completed in {str(timedelta(seconds=int(train_time)))}")
 
     # Save model(s) and metadata to disk if requested
+    # TODO check metadata saving !!!
     if args.save:
         metadata = {
             "mode": args.mode,
             "dataset": dataset.config,
-            "model": model.config,
-            "scheme": scheme_type,
+            "model": None,  # TODO
+            "scheme": None,  # TODO
         }
-        save_models(Path("models"), model_list, metadata, dataset.basis)
+        save_models(Path("models"), model_list, metadata, dataset.basis)  # TODO
 
 
 ###############################################################################
@@ -74,7 +77,7 @@ def _setup_datasource(config: Dict) -> DataSource:
     """
     Setup the data source from config
     """
-    datasource_type = config[f"datasource"]["type"]
+    datasource_type = config[f"datasource"]["datasource_type"]
     logging.info(f"Initialising data source: {datasource_type}")
     datasource = DataSourceRegistry.get(datasource_type)(**config["datasource"])
 
@@ -85,7 +88,7 @@ def _setup_dataset(config: Dict, mode: Mode, datasource: DataSource) -> Dataset:
     """
     Process the dataset using input configuration or load an existing one from disk
     """
-    dataset_type = config["dataset"]["type"]
+    dataset_type = config["dataset"]["dataset_type"]
     logging.info(f"Initialising training dataset: {dataset_type}")
     dataset = DatasetRegistry.get(dataset_type)(**config["dataset"], mode=mode, datasource=datasource)
     dataset.process()
@@ -97,59 +100,25 @@ def _setup_dataset(config: Dict, mode: Mode, datasource: DataSource) -> Dataset:
     return dataset
 
 
-def _setup_model(config: Dict, dataset: Dataset) -> Model:
+def _setup_strategy(config: Dict, dataset: Dataset) -> Strategy:
     """
-    Initialises or loads the model and its descriptors.
+    Initialises the learning strategy.
     """
+    strategy_config = config["strategy"]
+    strategy_type = strategy_config["strategy_type"]
+
     model_config = config["model"]
-    model_type = model_config["type"]
+    learner_config = config["learner"]
 
-    if hasattr(PretrainedModels, model_type):
-        # TODO -----------------------------------------------------
-        # TODO check pretrained model loading
-        # TODO also check if descriptors are loaded correctly
-        logging.info(f"Loading pretrained model: {model_type}")
-        model_params = model_config.get("params", {})
-        model = load_pretrained_model(model_type, **model_params)
-        # TODO -----------------------------------------------------
-    else:
-        logging.info(f"Initialising model: {model_type}")
-        model_params = model_config.get("params", {})
-        model = ModelRegistry.get(model_type)(**model_params, **dataset.metadata)
+    logging.info(f"Initialising strategy: {strategy_type}")
+    strategy = StrategyRegistry.get(strategy_type)(
+        **strategy_config,
+        dataset=dataset,
+        model_config=model_config,
+        learner_config=learner_config,
+    )
 
-        # Intialise model weights
-        weights_params = model_config.get("weights_params", {})
-        weights_init = model_config.get("weights_init", {}).get("weights", "default")
-        bias_init = model_config.get("weights_init", {}).get("bias", "zeros")
-        logging.info(f"Initialising weights with '{weights_init}' and bias with '{bias_init}'")
-        model.init_weights(weights_init, bias_init, **weights_params)
-
-    return model
-
-
-def _setup_scheme(config: Dict, args, model: Model, dataset: Dataset) -> Learn:
-    """
-    Initialise training scheme (standard, k-fold, ensemble, etc.).
-    """
-    model_type = config["model"].get("type")
-
-    logging.info("Initialising training scheme")
-    # Pack parameters
-    kwargs = {
-        "model_config": config.get("model"),
-        "hyper_params": config.get("hyperparams", {}),
-        "earlystop_params": config.get("earlystop_params", {}),
-        "kfold_params": config.get("kfold_params", {}),
-        "bootstrap_params": config.get("bootstrap_params", {}),
-        "ensemble_params": config.get("ensemble_params", {}),
-        "lr_scheduler": config.get("lr_scheduler", False),
-        "scheduler_params": config.get("scheduler_params", {}),
-        "mlflow": args.mlflow,
-        "tensorboard": args.tensorboard,
-    }
-
-    scheme = SchemeRegistry.get_learn(model_type)(model, dataset, **kwargs)
-    return scheme
+    return strategy
 
 
 ###############################################################################
@@ -157,29 +126,13 @@ def _setup_scheme(config: Dict, args, model: Model, dataset: Dataset) -> Learn:
 ###############################################################################
 
 
-def _run_training(config: Dict, scheme: Learn) -> Tuple[List, str, float]:
+def _run_training(strategy: Strategy) -> Tuple[List, float]:
     """
     Train using the selected training scheme.
     """
-    model_list = []
     start_time = time.time()
 
-    if config["bootstrap"]:
-        logging.info("Training model using bootstrap resampling...")
-        scheme_type = "bootstrap"
-        model_list = scheme.train_bootstrap()
-    elif config["ensemble"]:
-        logging.info("Training model using ensemble learning...")
-        scheme_type = "ensemble"
-        model_list = scheme.train_ensemble()
-    elif config["kfold"]:
-        logging.info("Training model using kfold cross-validation...")
-        scheme_type = "kfold"
-        model_list.append(scheme.train_kfold())
-    else:
-        logging.info("Training model using standard training procedure...")
-        scheme_type = "std"
-        model_list.append(scheme.train_std())
+    model_list = strategy.run_training()
 
     train_time = time.time() - start_time
 
@@ -187,7 +140,7 @@ def _run_training(config: Dict, scheme: Learn) -> Tuple[List, str, float]:
     for model in model_list:
         model.to(torch.device("cpu"))
 
-    return model_list, scheme_type, train_time
+    return model_list, train_time
 
 
 ###############################################################################
@@ -195,6 +148,7 @@ def _run_training(config: Dict, scheme: Learn) -> Tuple[List, str, float]:
 ###############################################################################
 
 
+# TODO Check this !!!
 def _summary_model(model: Model, dataset: Dataset) -> None:
     logging.info("--- Model Summary ---")
 
